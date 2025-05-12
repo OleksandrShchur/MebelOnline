7c837ff8a6179a923adc8a37a5b562d67875a572
Oleksandr Shchur <osur12709@gmail.com>
2025-05-12 21:44:06 +0300

    change column length for product fields

--- MebelOnline.Db/Migrations/Up/001_AddTables.sql
+++ MebelOnline.Db/Migrations/Up/001_AddTables.sql
@@ -23,10 +23,10 @@ BEGIN
     CREATE TABLE dbo.Products (
         Id INT PRIMARY KEY IDENTITY(1,1),
         Name NVARCHAR(255) NOT NULL,
-        Description NVARCHAR(MAX) NULL,
+        Description NVARCHAR(500) NULL,
         Price DECIMAL(18, 2) NOT NULL,
         StockQuantity INT NOT NULL,
-        ImageUrl NVARCHAR(MAX) NULL,
+        ImageUrl NVARCHAR(500) NULL,
         DateAdded DATETIME NOT NULL,
         CategoryId INT NOT NULL,
         CONSTRAINT FK_Products_Categories FOREIGN KEY (CategoryId) REFERENCES dbo.Categories(Id) ON DELETE CASCADE
