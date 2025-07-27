5ba5c50acc06f0f13a732dc325a194b4bc4b1ab7
Oleksandr Shchur <osur12709@gmail.com>
2025-07-27 16:15:49 +0300

    split desktop and mobile layout

--- mebelonline.client/src/components/productAllDetails/productAllDetails.tsx
+++ mebelonline.client/src/components/productAllDetails/productAllDetails.tsx
@@ -1,13 +1,14 @@
-import { Box, Grid } from "@mui/material";
-import ProductInfoCard from "../productInfoCard/productInfoCard";
+import { useMediaQuery, useTheme } from "@mui/material";
 import ProductImageModal from "../productImageModal/productImageModal";
 import { useState } from "react";
 import type { ProductDetailsModel } from "../../models/productDetailsModel";
 import { type EmblaOptionsType } from "embla-carousel";
-import ImageCarousel from "../imageCarousel/imageCarousel";
-import ProductDescriptionCard from "../productDescriptionCard/productDescriptionCard";
+import { ProductAllDetailsDesktop } from "./responsive/productAllDetailsDesktop";
+import { ProductAllDetailsMobile } from "./responsive/productAllDetailsMobile";
 
 const OPTIONS: EmblaOptionsType = { loop: true };
+const DESKTOP_RIGHT_MARGIN = 8;
+const MOBILE_RIGHT_MARGIN = 0;
 
 interface IProductAllDetailsProps {
     productDetails: ProductDetailsModel;
@@ -17,6 +18,11 @@ const ProductAllDetails: React.FC<IProductAllDetailsProps> = (props: IProductAll
     const { productDetails } = props;
     const [isModalOpen, setModalOpen] = useState<boolean>(false);
     const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0);
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+    const imageMargin = isDesktop
+        ? DESKTOP_RIGHT_MARGIN
+        : MOBILE_RIGHT_MARGIN;
 
     const handleModalClose = () => {
         setModalOpen(false);
@@ -30,8 +36,20 @@ const ProductAllDetails: React.FC<IProductAllDetailsProps> = (props: IProductAll
     return (
         <>
             <ProductImageModal isOpen={isModalOpen} handleClose={handleModalClose} images={productDetails.images}
-                startIndex={selectedImageIndex} />
-            <Grid container spacing={4}>
+                startIndex={selectedImageIndex} rightMargin={imageMargin} />
+            {isDesktop &&
+                <ProductAllDetailsDesktop
+                    productDetails={productDetails}
+                    options={OPTIONS}
+                    handleImageClick={handleImageClick} />
+            }
+            {!isDesktop &&
+                <ProductAllDetailsMobile
+                    productDetails={productDetails}
+                    options={OPTIONS}
+                    handleImageClick={handleImageClick} />
+            }
+            {/* <Grid container spacing={4}>
                 <Grid size={6}>
                     <Box>
                         <ProductInfoCard id={productDetails.id}
@@ -61,7 +79,7 @@ const ProductAllDetails: React.FC<IProductAllDetailsProps> = (props: IProductAll
                         />
                     </Box>
                 </Grid>
-            </Grid>
+            </Grid> */}
         </>
     );
 };
---
+++ mebelonline.client/src/components/productAllDetails/responsive/productAllDetailsMobile.tsx
@@ -0,0 +1,50 @@
+import { Box, Grid } from "@mui/material";
+import ProductDescriptionCard from "../../productDescriptionCard/productDescriptionCard";
+import ImageCarousel from "../../imageCarousel/imageCarousel";
+import type { ProductDetailsModel } from "../../../models/productDetailsModel";
+import type { EmblaOptionsType } from "embla-carousel";
+import ProductInfoCard from "../../productInfoCard/productInfoCard";
+
+interface IProductAllDetailsMobileProps {
+    productDetails: ProductDetailsModel;
+    options: EmblaOptionsType;
+    handleImageClick: (index: number) => void;
+};
+
+export const ProductAllDetailsMobile: React.FC<IProductAllDetailsMobileProps> = (props: IProductAllDetailsMobileProps) => {
+    const { productDetails, options, handleImageClick } = props;
+
+    return (
+        <Grid container spacing={4}>
+            <Grid size={12}>
+                <Box sx={{ position: 'sticky', top: 80, zIndex: 1000, pb: 2 }}>
+                    <ImageCarousel images={productDetails.images} options={options} handleOpen={handleImageClick}
+                        slideHeight="60vh"
+                    />
+                </Box>
+            </Grid>
+            <Grid size={12}>
+                <Box>
+                    <ProductInfoCard id={productDetails.id}
+                        title={productDetails.title}
+                        price={productDetails.price}
+                        oldPrice={productDetails.oldPrice}
+                        note={productDetails.note}
+                        frontOptions={productDetails.frontOptions}
+                        frameOptions={productDetails.frameOptions}
+                    />
+                </Box>
+                <Box>
+                    <ProductDescriptionCard
+                        title={productDetails.title}
+                        width={productDetails.width}
+                        height={productDetails.height}
+                        depth={productDetails.depth}
+                        description={productDetails.description}
+                        attributes={productDetails.attributes}
+                    />
+                </Box>
+            </Grid>
+        </Grid>
+    );
+}
