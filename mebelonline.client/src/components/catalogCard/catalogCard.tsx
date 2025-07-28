364975f05d523fbec6685f2929bdca52341789eb
Oleksandr Shchur <osur12709@gmail.com>
2025-07-28 20:21:27 +0300

    show subcategories in catalog card

--- mebelonline.client/src/components/catalogCard/catalogCard.tsx
+++ mebelonline.client/src/components/catalogCard/catalogCard.tsx
@@ -11,7 +11,7 @@ const CatalogCard: React.FC<ICatalogCardProps> = (props: ICatalogCardProps) => {
     return (
         <Card
             sx={{
-                position: 'relative', maxWidth: 400, height: 350, mx: 'auto', display: 'flex', flexDirection: 'column',
+                position: 'relative', maxWidth: 400, height: 370, mx: 'auto', display: 'flex', flexDirection: 'column',
                 boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.15)',
                 borderRadius: 2,
                 transition: 'box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out',
@@ -33,18 +33,37 @@ const CatalogCard: React.FC<ICatalogCardProps> = (props: ICatalogCardProps) => {
                 <Link underline="none" href={`/category/${category.id}`} color="inherit">
                     <CardContent sx={{ flexGrow: 1, px: 2, py: 1 }}>
                         <Tooltip title={category.name} arrow>
-                            <Typography variant="body2" fontWeight={500} gutterBottom
+                            <Typography variant="subtitle1" fontWeight={500} gutterBottom
+                                sx={{
+                                    display: '-webkit-box',
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    WebkitBoxOrient: 'vertical',
+                                }}
+                            >
+                                {category.name}
+                            </Typography>
+                        </Tooltip>
+                        {Array.from({ length: 4 }, (_, index) => category.subCategories[index] || { name: '\u00A0' }).map((sub, index) => (
+                            <Typography key={index} variant="caption" gutterBottom
                                 sx={{
                                     display: '-webkit-box',
                                     overflow: 'hidden',
                                     textOverflow: 'ellipsis',
                                     WebkitBoxOrient: 'vertical',
                                     WebkitLineClamp: 1, // number of lines shown
+                                    minHeight: '1.5em', // Ensures consistent height for empty lines
                                 }}
                             >
-                                {category.name}
+                                {sub.name}
                             </Typography>
-                        </Tooltip>
+                        ))}
+                        <Box sx={{ textAlign: 'right' }}>
+                            <Typography variant="button">
+                                Показати всі...
+                            </Typography>
+                        </Box>
+
                     </CardContent>
                 </Link>
             </Box>
