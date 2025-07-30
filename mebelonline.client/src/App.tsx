9aef323c1c7b39de8ad7070c903a392ce2ae6219
Oleksandr Shchur <71557324+OleksandrShchur@users.noreply.github.com>
2025-07-30 18:50:44 +0300

    add theme provider to the application (#31)

--- mebelonline.client/src/App.tsx
+++ mebelonline.client/src/App.tsx
@@ -5,18 +5,22 @@ import Home from './pages/home/home';
 import Layout from './components/layout/layout';
 import ProductDetails from './pages/productDetails/productDetails';
 import Catalog from './pages/catalog/catalog';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme/theme';
 
 const App: React.FC = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path='/catalog' element={<Catalog />} />
-                    <Route path='/product/:productId' element={<ProductDetails />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path='/catalog' element={<Catalog />} />
+                        <Route path='/product/:productId' element={<ProductDetails />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
