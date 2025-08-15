6f89b6679aa53e2a92ff13d0eccd66b07a82d827
Oleksandr Shchur <osur12709@gmail.com>
2025-08-15 17:16:57 +0300

    create service provider for custom mappers

---
+++ MebelOnline.Core/Mappings/Config/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MebelOnline.Core.Mappings.Config
+{
+    public class Mapper : IMapper
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public Mapper(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public TTarget Map<TSource, TTarget>(TSource source)
+        {
+            var specificMapper = _serviceProvider.GetRequiredService<IMappingService<TSource, TTarget>>();
+            return specificMapper.Map(source);
+        }
+
+        public IList<TTarget> MapList<TSource, TTarget>(IList<TSource> source)
+        {
+            var specificMapper = _serviceProvider.GetRequiredService<IMappingService<TSource, TTarget>>();
+            return specificMapper.MapList(source);
+        }
+    }
+}
