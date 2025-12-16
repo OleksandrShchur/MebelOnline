fc34c00369e9d753032d54db3063efa671a97df9
Oleksandr Shchur <osur12709@gmail.com>
2025-12-16 21:59:49 +0200

    fix category list view

--- mebelonline.client/src/components/catalogCard/catalogCard.tsx
+++ mebelonline.client/src/components/catalogCard/catalogCard.tsx
@@ -5,70 +5,92 @@ const SUBCATEGORIES_COUNT = 4;
 
 interface ICatalogCardProps {
     category: CatalogModel;
-};
+}
 
-const CatalogCard: React.FC<ICatalogCardProps> = (props: ICatalogCardProps) => {
-    const { category } = props;
-
+const CatalogCard: React.FC<ICatalogCardProps> = ({ category }) => {
     return (
         <Card
             sx={{
-                position: 'relative', maxWidth: 400, height: 370, mx: 'auto', display: 'flex', flexDirection: 'column',
-                boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.15)',
+                position: "relative",
+                maxWidth: 400,
+                mx: "auto",
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.15)",
                 borderRadius: 2,
-                transition: 'box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out',
-                '&:hover': {
-                    boxShadow: '0px 6px 20px rgba(0, 0, 0, 0.3)',
-                    transform: 'translateY(-4px)',
+                transition: "box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out",
+                "&:hover": {
+                    boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.3)",
+                    transform: "translateY(-4px)",
                 },
-            }}>
+            }}
+        >
             <Link underline="none" href={`/category/${category.id}`} color="inherit">
                 <CardMedia
                     component="img"
-                    height="200"
                     image={category.imageUrl}
                     alt={category.name}
+                    sx={{
+                        aspectRatio: "16 / 9",
+                        objectFit: "cover",
+                    }}
                 />
             </Link>
 
-            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardContent
+                sx={{
+                    px: 2,
+                    py: 1,
+                    flexGrow: 1,
+                    '&:last-child': {
+                        paddingBottom: 1,
+                    },
+                }}>
                 <Link underline="none" href={`/category/${category.id}`} color="inherit">
-                    <CardContent sx={{ flexGrow: 1, px: 2, py: 1 }}>
-                        <Tooltip title={category.name} arrow>
-                            <Typography variant="subtitle1" fontWeight={500} gutterBottom
-                                sx={{
-                                    display: '-webkit-box',
-                                    overflow: 'hidden',
-                                    textOverflow: 'ellipsis',
-                                    WebkitBoxOrient: 'vertical',
-                                }}
-                            >
-                                {category.name}
-                            </Typography>
-                        </Tooltip>
-                        {Array.from({ length: SUBCATEGORIES_COUNT }, (_, index) =>
-                            category.subCategories[index] || { name: '\u00A0' }).map((sub, index) => (
-                                <Typography key={index} variant="caption" gutterBottom
-                                    sx={{
-                                        display: '-webkit-box',
-                                        overflow: 'hidden',
-                                        textOverflow: 'ellipsis',
-                                        WebkitBoxOrient: 'vertical',
-                                        minHeight: '1.5em', // Ensures consistent height for empty lines
-                                    }}
-                                >
-                                    {sub.name}
-                                </Typography>
-                            ))}
-                        <Box sx={{ textAlign: 'right' }}>
-                            <Typography variant="button">
-                                Показати всі...
-                            </Typography>
-                        </Box>
-
-                    </CardContent>
+                    <Tooltip title={category.name} arrow>
+                        <Typography
+                            variant="subtitle1"
+                            fontWeight={500}
+                            gutterBottom
+                            sx={{
+                                display: "-webkit-box",
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                                WebkitBoxOrient: "vertical",
+                                WebkitLineClamp: 2,
+                            }}
+                        >
+                            {category.name}
+                        </Typography>
+                    </Tooltip>
                 </Link>
-            </Box>
+
+                {Array.from({ length: SUBCATEGORIES_COUNT }, (_, index) =>
+                    category.subCategories[index] || { name: "\u00A0" }
+                ).map((sub, index) => (
+                    <Typography
+                        key={index}
+                        variant="caption"
+                        gutterBottom
+                        sx={{
+                            display: "-webkit-box",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            WebkitBoxOrient: "vertical",
+                            WebkitLineClamp: 1,
+                            minHeight: "1.5em",
+                        }}
+                    >
+                        {sub.name}
+                    </Typography>
+                ))}
+
+                <Box sx={{ textAlign: "right", mt: 1 }}>
+                    <Link underline="none" href={`/category/${category.id}`} color="inherit">
+                        <Typography variant="button">Показати всі...</Typography>
+                    </Link>
+                </Box>
+            </CardContent>
         </Card>
     );
 };
